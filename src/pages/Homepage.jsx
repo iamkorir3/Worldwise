f01948d7baec7f055d86e3ea912c30ccdddbfee4
iamkorir3 <korir5956@gmail.com>
2025-07-24 20:27:31 +0300

    building actual pages

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,17 +1,20 @@
-import { Link } from "react-router-dom";
-import PageNavigation from "../components/PageNavigation";
-import AppLayout from "./AppLayout";
+import styles from "./Homepage.module.css";
 
-function Homepage() {
+export default function Homepage() {
   return (
-    <div>
-      <PageNavigation />
-      <h1>WorldWise</h1>
-      <Link to="app" element={<AppLayout />}>
-        Go to App
-      </Link>
-    </div>
+    <main className={styles.homepage}>
+      <section>
+        <h1>
+          You travel the world.
+          <br />
+          WorldWise keeps track of your adventures.
+        </h1>
+        <h2>
+          A world map that tracks your footsteps into every city you can think
+          of. Never forget your wonderful experiences, and show your friends how
+          you have wandered the world.
+        </h2>
+      </section>
+    </main>
   );
 }
-
-export default Homepage;
