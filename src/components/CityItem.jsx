9506c21082837200df4f4aac8907106444d5b590
iamkorir3 <korir5956@gmail.com>
2025-07-26 23:41:52 +0300

    finished cities

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -15,10 +15,11 @@ function CityItem({ city }) {
   const { cityName, emoji, date } = city;
 
   return (
-    <li className={styles.CityItem}>
+    <li className={styles.cityItem}>
       <span className={styles.emoji}> {emoji}</span>
-      <h3 className={styles.emoji}> {cityName}</h3>
-      <time className={styles.date}> {formatDate(date)}</time>
+      <h3 className={styles.name}> {cityName}</h3>
+      <time className={styles.date}>( {formatDate(date)})</time>
+      <button className={styles.deleteBtn}>&times;</button>
     </li>
   );
 }
