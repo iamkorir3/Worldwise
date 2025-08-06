de469df19d56e3bf11064c8a7644937f3a39cd71
iamkorir3 <korir5956@gmail.com>
2025-08-06 13:26:51 +0300

    degging

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css";
+import { useCities } from "./contexts/CitiesContext";
 // import PropTypes from "prop-types";
 
 const formatDate = (date) =>
@@ -11,13 +12,17 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function CityItem({ city }) {
+  const { currentCity } = useCities();
   const { cityName, emoji, date, id, position } = city;
 
   return (
     <li>
       <Link
-        className={styles.cityItem}
+        className={`${styles.cityItem} ${
+          id === currentCity.id ? styles["cityItem--active"] : ""
+        }`}
         to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+        onClick={console.log(position.lat)}
       >
         <span className={styles.emoji}> {emoji}</span>
         <h3 className={styles.name}> {cityName}</h3>
